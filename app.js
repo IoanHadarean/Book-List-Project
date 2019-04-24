5651491e4c5f5e30764b77aaf84797c38ab0b4cf
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-04-24 21:17:07 +0000

    Appended the form inputs to the book list table

--- app.js
+++ app.js
@@ -6,17 +6,25 @@ function Book(title, author, isbn) {
 }
 
 
-
-
-
-
 // UI Constructor
 function UI() {}
 
 
 // Add Book To List
 UI.prototype.addBookToList = function(book) {
-    console.log(book);
+    const list = document.getElementById('book-list');
+    // Create tr element
+    const row = document.createElement('tr');
+    
+    // Insert cols
+    row.innerHTML = `<td>${book.title}</td>
+                    <td>${book.author}</td>
+                    <td>${book.isbn}</td>
+                    <td><a href = "#" class = "delete">X</a></td>`
+    
+    console.log(row);
+    
+    list.appendChild(row);
 }
 
 
