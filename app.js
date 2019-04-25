953dbbefb1d88b666dff72787e986f60e007d4ee
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-04-25 21:23:52 +0000

    Created delete functionality for book list

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ UI.prototype.addBookToList = function(book) {
                      <td>${book.isbn}</td>
                      <td><a href = "#" class = "delete">X</a></td>`;
 
-    console.log(row);
-
     list.appendChild(row);
 };
 
@@ -49,6 +47,13 @@ UI.prototype.showAlert = function(message, className) {
     }, 3000);
 };
 
+// Delete Book
+UI.prototype.deleteBook = function(target) {
+    if(target.className === 'delete') {
+        target.parentElement.parentElement.remove();
+    }
+};
+
 // Clear Fields
 UI.prototype.clearFields = function() {
     document.getElementById('title').value = '';
@@ -59,7 +64,7 @@ UI.prototype.clearFields = function() {
 
 
 
-// Event Listeners
+// Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
     // Get form values
     const title = document.getElementById('title').value,
@@ -68,10 +73,11 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
 
     // Instantiate book
     const book = new Book(title, author, isbn);
-    console.log(book);
 
     //Instantiate UI
     const ui = new UI();
+    
+    console.log(ui);
 
 
     // Validate
@@ -90,6 +96,19 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
         ui.clearFields();
     }
 
-    console.log(title, author, isbn);
+    e.preventDefault();
+});
+
+// Event listener for delete
+document.getElementById('book-list').addEventListener('click', function(e) {
+    
+    const ui = new UI();
+    
+    // Delete book
+    ui.deleteBook(e.target);
+
+    // Show message
+    ui.showAlert('Book Removed!', 'success');
+
     e.preventDefault();
 });
